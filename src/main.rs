e74c1ae59ce99f738525f2a662e567664be89f31
Fabien JUIF <fabien.juif@gmail.com>
2019-03-02 13:37:16 +0100

    :sparkles: remove disconnected worker

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use zmq::{self, SocketType};
-use std::time::SystemTime;
-use std::slice::Iter;
 use std::collections::HashMap;
+use std::slice::Iter;
+use std::time::SystemTime;
+use zmq::{self, SocketType};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Client {
     name: String,
     is_worker: bool,
@@ -21,19 +21,19 @@ impl Client {
 }
 
 #[derive(Debug)]
-struct Topic<'a> {
+struct Topic {
     name: String,
     workers: Vec<String>,
-    workers_iterator: Option<Iter<'a, String>>,
+    // workers_iterator: Option<Iter<'a, String>>,
     clients: Vec<String>,
 }
 
-impl<'a> Topic<'a> {
-    fn new(name: &str) -> Topic<'a> {
+impl Topic {
+    fn new(name: &str) -> Topic {
         Topic {
             name: name.to_string(),
             workers: vec![],
-            workers_iterator: None,
+            // workers_iterator: None,
             clients: vec![],
         }
     }
@@ -47,6 +47,7 @@ struct Task {
     retry: u8,
     payload: String,
     date: SystemTime,
+    sent: bool,
 }
 
 impl Task {
@@ -58,18 +59,110 @@ impl Task {
             retry: 0,
             payload: payload.to_string(),
             date: SystemTime::now(),
+            sent: false,
         }
     }
 }
 
-// TODO: should be accessible from a dedicated socket and only when the client ask for it
-//       it will speed up the overall process since it wouldn't have to use stdout for each task
-fn print_debug(all_clients: &HashMap<String, Client>, topics: &HashMap<String, Topic>, tasks: &Vec<Task>) {
-    let (workers, clients): (Vec<&Client>, Vec<&Client>) = all_clients
-        .values()
-        .partition(|&client| client.is_worker);
+struct Broker {
+    clients: HashMap<String, Client>,
+    topics: HashMap<String, Topic>,
+    tasks: Vec<Task>,
+}
 
-    println!("[{} workers; {} clients; {} topics; {} tasks]", &workers.len(), &clients.len(), &topics.len(), &tasks.len());
+impl Broker {
+    fn new() -> Broker {
+        Broker {
+            clients: HashMap::new(),
+            topics: HashMap::new(),
+            tasks: Vec::new(),
+        }
+    }
+
+    fn get_next_worker_name(&self, topic_name: &str) -> String {
+        // TODO: round robin
+        let topic = self.topics.get(topic_name).unwrap();
+        dbg!(&topic.workers);
+        let worker = self.clients.get(topic.workers.get(0).unwrap()).unwrap();
+
+        worker.name.clone()
+    }
+
+    fn add_client(&mut self, is_worker: bool, identity: &str, response_topic: &str) {
+        // add client
+        let client = self
+            .clients
+            .entry(identity.to_string())
+            .or_insert(Client::new(&identity, is_worker));
+        client.topics.push(response_topic.to_string());
+
+        // add topic
+        let topic = self
+            .topics
+            .entry(response_topic.to_string())
+            .or_insert(Topic::new(&response_topic));
+        if is_worker {
+            topic.workers.push(identity.to_string());
+        } else {
+            topic.clients.push(identity.to_string());
+        }
+    }
+
+    fn send_task(&self, mut socket: &zmq::Socket, mut task: Task) -> Task {
+        task.date = SystemTime::now();
+        task.retry += 1;
+
+        if task.retry >= 3 {
+            // TODO: make it a const
+            panic!("Max retry!");
+        }
+
+        // select a worker
+        let worker_name = self.get_next_worker_name(&task.worker_topic);
+        task.worker_name = Some(worker_name.clone());
+
+        // send the task to the worker
+        // if it doesn't works (worker is dead for instance), then we retry
+        // the recursion is done if there is no worker anymore or if the retry is to damn high
+        let sent = socket
+            .send(&worker_name, zmq::SNDMORE | zmq::DONTWAIT)
+            .and_then(|_| socket.send("", zmq::SNDMORE | zmq::DONTWAIT))
+            .and_then(|_| socket.send(&task.payload, zmq::DONTWAIT));
+        task.sent = sent.is_ok();
+
+        task
+    }
+
+    fn remove_worker_from_topics(&mut self, worker: &Client) {
+        worker.topics.iter().for_each(|topic| {
+            self.topics.entry(topic.to_string()).and_modify(|topic| {
+                let position = topic.workers.iter().position(|name| name == &worker.name);
+                topic.workers.remove(position.unwrap());
+            });
+        });
+    }
+
+    fn remove_worker(&mut self, worker_name: &str) {
+        let worker = self.clients.get(worker_name).unwrap();
+        let worker = worker.clone(); // FIXME:
+        self.remove_worker_from_topics(&worker);
+        self.clients.remove(worker_name);
+    }
+
+    // TODO: should be accessible from a dedicated socket and only when the client ask for it
+    //       it will speed up the overall process since it wouldn't have to use stdout for each task
+    fn print_debug(&self) {
+        let (workers, clients): (Vec<&Client>, Vec<&Client>) =
+            self.clients.values().partition(|&client| client.is_worker);
+
+        println!(
+            "[{} workers; {} clients; {} topics; {} tasks]",
+            &workers.len(),
+            &clients.len(),
+            &self.topics.len(),
+            &self.tasks.len()
+        );
+    }
 }
 
 // TODO: don't use strings
@@ -83,16 +176,13 @@ fn main() {
 
     let mut message = zmq::Message::new();
 
+    let mut broker = Broker::new();
+
     let mut index = 0;
     let mut identity = String::from("");
     let mut topic = String::from("");
     let mut response_topic = String::from("");
     let mut payload = String::from("");
-
-    // TODO: change collection
-    let mut clients = HashMap::<String, Client>::new();
-    let mut topics = HashMap::<String, Topic>::new();
-    let mut tasks = vec![];
 
     loop {
         socket.recv(&mut message, 0).unwrap();
@@ -106,72 +196,31 @@ fn main() {
             _ => panic!(format!("Unknown index for message: {}", index)),
         }
 
-        let mut add_client = |is_worker| {
-            // add client
-            let client = clients
-                .entry(identity.clone())
-                .or_insert(Client::new(&identity, is_worker));
-            client.topics.push(response_topic.clone());
-
-            // add topic
-            let topic = topics
-                .entry(response_topic.clone())
-                .or_insert(Topic::new(&response_topic));
-            if is_worker {
-                topic.workers.push(identity.clone());
-            } else {
-                topic.clients.push(identity.clone());
-            }
-        };
-
-        let get_next_worker_name = |topic_name: &str, topics: &HashMap<String, Topic>, clients: &HashMap<String, Client>| {
-            // TODO: round robin
-            let topic = topics.get(topic_name).unwrap();
-            let worker = clients.get(topic.workers.get(0).unwrap()).unwrap();
-
-            worker.name.clone()
-        };
-
-        let send_task = |mut task: Task, topics: &HashMap<String, Topic>, clients: &HashMap<String, Client>| {
-            task.date = SystemTime::now();
-            task.retry += 1;
-
-            if task.retry >= 3 { // TODO: make it a const
-                panic!("Max retry!");
-            }
-
-            // select a worker
-            let worker_name = get_next_worker_name(&task.worker_topic, topics, clients);
-            task.worker_name = Some(worker_name.clone());
-
-            // send the task to the worker
-            // if it doesn't works (worker is dead for instance), then we retry
-            // the recursion is done if there is no worker anymore or if the retry is to damn high
-            dbg!(&task);
-            // socket.send_multipart(&vec![worker_name.as_bytes(), task.payload.as_bytes()], zmq::DONTWAIT);
-            socket.send(&worker_name, zmq::SNDMORE | zmq::DONTWAIT).expect("send should work 1");
-            socket.send("", zmq::SNDMORE | zmq::DONTWAIT).expect("send should work 2"); // TODO: this could be removed!
-            socket.send(&task.payload, zmq::DONTWAIT).expect("send should work 3");
-            // TODO: const sent = send(sock, [worker.name, '', task.payload])
-            // TODO: if (!sent) return sendTask(sock, task)
-        };
-
         if message.get_more() {
             index += 1;
         } else {
             index = 0;
 
             if topic.as_str() == "@@REGISTER" {
-                add_client(true);
+                broker.add_client(true, &identity, &response_topic);
             } else {
-                add_client(false);
-                let task = Task::new(&topic, &response_topic, &payload);
-                send_task(task, &topics, &clients);
+                broker.add_client(false, &identity, &response_topic);
+                let mut task = Task::new(&topic, &response_topic, &payload);
+                loop {
+                    task = broker.send_task(&socket, task);
+                    if !task.sent {
+                        dbg!(&task);
+                        let worker_name = task.worker_name.as_ref().unwrap();
+                        broker.remove_worker(&worker_name);
+                    } else {
+                        break;
+                    }
+                }
                 // TODO: add task to vec
             }
             // TODO: handle worker response
 
-            print_debug(&clients, &topics, &tasks);
+            broker.print_debug();
         }
     }
 }
