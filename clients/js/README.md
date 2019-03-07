4ccf038c85be2369a5ec40defbde222e68fa2530
Fabien JUIF <fabien.juif@gmail.com>
2019-03-07 11:28:45 +0100

    :sparkles: js client

---
+++ clients/js/README.md
@@ -0,0 +1,61 @@
+# @tiny-broke/client
+
+## Service
+```js
+import connect from '@tiny-broke/client'
+
+// connect to tiny-broke
+const broke = connect(
+  'invoices', // service name
+  'tcp://localhost:3000', // tiny-broke uri
+  true, // is it a worker? yes
+)
+
+// your service
+const get = async (action) => {
+  // this is here you are doing your stuff, like requesting your DB
+
+  return {
+    id: action.payload,
+    price: 2342,
+  }
+}
+
+// you register your `get` service behind `INVOICES>GET` event
+// when tiny broke recieve the `INVOICES>GET` event it will call the logger callback first
+broke.register(
+  'INVOICES>GET', // register to this event type
+  get, // calls this service to process the data and return a response
+  (action) => console.log('getting invoice by id', JSON.stringify(action)), // log this when the event arrives (can be omitted)
+)
+```
+
+## Client
+```js
+import connect from '@tiny-broke/client'
+
+// connect to tiny-broke
+const broke = connect(
+  'graphql-api', // client name
+  'tcp://localhost:3000', // tiny-broke uri
+  false, // is it a worker? no this is a client
+)
+
+// event factory helper
+const getInvoice: id => ({
+  type: 'INVOICES>GET', // event type, must match a register from a service
+  returnsType: `INVOICES>GET>${id}`, // this type is used to tells tiny-broke that we are waiting this response to complete
+  payload: id, // whatever you want in payload, here the invoice id
+})
+
+const run = async (id) => {
+  // ask a worker to process the `INVOICES>GET` event
+  // and wait for the result
+  const invoice = await broke.wait(getInvoice(id))
+
+  // this is here you put your further processing
+  console.log(invoice)
+}
+
+run(10)
+```
---
+++ clients/js/src/index.ts
@@ -0,0 +1,160 @@
+const zmq = require('zeromq')
+
+interface ZMQSocket {
+  identity: string,
+  connect: (uri: string) => void,
+  close: () => void,
+  on: (type: string, callback: (...args: Buffer[]) => void) => void,
+  send: (message: Array<string | Buffer>) => void
+}
+
+const create = (name = '', uri: string, isWorker = false) => {
+  let sock: ZMQSocket
+
+  const sendResponse = (action: { type: string }) => {
+    sock.send([action.type, '', JSON.stringify(action)])
+  }
+
+  interface Registration {
+    type: string,
+    callback: Function,
+    log: Function,
+  }
+  const registrations = new Map<string, Registration>()
+
+  const sendRegistrations = () => {
+    if (!isWorker) return
+
+    registrations.forEach((_, type) => {
+      sock.send(['@@REGISTER', type])
+    })
+  }
+
+  const start = () => {
+    if (sock) sock.close()
+    sock = zmq.socket('dealer')
+    sock.identity = `${isWorker ? 'worker' : 'client'}-${name}-${process.pid}` // FIXME: in a container all process id would be same ?? use uuid
+    sock.connect(uri)
+
+    sendRegistrations()
+    ping()
+
+    sock.on('message', async (_, messageBuffer) => {
+      const message = messageBuffer.toString()
+
+      // heart beating
+      if (message === '@@PONG') {
+        pongRecieved = true
+        ping()
+        return
+      } else if (message === '@@REGISTER') {
+        sendRegistrations()
+        ping()
+        return
+      }
+
+      // find the associated registrations
+      const action = JSON.parse(message)
+      const registration = registrations.get(action.type)
+      if (!registration) {
+        ping()
+        return
+      }
+
+      // call the binded service
+      const { log, callback } = registration
+      let error
+      let payload
+      try {
+        if (log) log(action)
+        payload = await callback(action)
+      } catch (ex) {
+        console.error(`error while responding to ${action.type}`, ex.message)
+        error = ex
+      }
+
+      // send reponse to broker
+      sendResponse({
+        ...action,
+        type: action.returnsType,
+        from: action.type,
+        payload,
+        error,
+      })
+
+      // reset heart beating
+      ping()
+    })
+  }
+
+  const PING_INTERVAL = 1000
+  const PING_TIMEOUT = 1000
+  let timerPing: NodeJS.Timeout
+  let timerPong: NodeJS.Timeout
+  let pongRecieved = false
+
+  const recievePong = () => {
+    if (timerPong) clearTimeout(timerPong)
+    timerPong = setTimeout(
+      () => {
+        if (!pongRecieved) {
+          console.log('Trying to reconnect...')
+          start()
+        }
+      },
+      PING_TIMEOUT,
+    )
+  }
+
+  const ping = () => {
+    if (timerPing) clearTimeout(timerPing)
+    timerPing = setTimeout(
+      () => {
+        sock.send(['@@PING'])
+        pongRecieved = false
+        recievePong()
+      },
+      PING_INTERVAL
+    )
+  }
+
+  const register = (type: string, callback: (action: { payload: any, error: string, from: string }) => void, log?: Function) => {
+    registrations.set(
+      type,
+      {
+        type,
+        callback,
+        log,
+      },
+    )
+
+    if (isWorker) sock.send(['@@REGISTER', type])
+  }
+
+  const wait = (action: { type: string, returnsType: string }) => {
+    sock.send([action.type, action.returnsType, JSON.stringify(action)])
+
+    return new Promise((resolve, reject) => {
+      register(
+        action.returnsType,
+        ({ payload, error, from }) => {
+          if (!error) return resolve(payload)
+
+          console.error(error)
+          const thrownError: any = new Error(error)
+          thrownError.from = from
+          return reject(thrownError)
+        },
+      )
+    })
+  }
+
+  start()
+
+  return {
+    register,
+    wait,
+  }
+}
+
+export default create
